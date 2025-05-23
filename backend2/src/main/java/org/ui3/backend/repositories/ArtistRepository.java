b3ee8497865ae26c325b4e0252516dc00b67c17a
knv22u635 <kazakovnv@student.bmstu.ru>
2025-05-24 00:33:37 +0300

    Laba8

--- backend2/src/main/java/org/ui3/backend/repositories/ArtistRepository.java
+++
@@ -1,9 +0,0 @@
-package org.ui3.backend.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.ui3.backend.models.Artist;
-
-@Repository
-public interface ArtistRepository extends JpaRepository<Artist, Long> {
-}
